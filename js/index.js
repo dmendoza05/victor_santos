a10c21e83430e2240e06f1136ece0d480c7a7666
Daniel Mendoza <mendozad05@yahoo.com>
2016-04-11 02:13:20 -0700

    updated site still incomplete( changed style and design, removed initial html, adding content on home html

--- js/index.js
+++ js/index.js
@@ -6,29 +6,23 @@
   app.config(function($stateProvider, $urlRouterProvider, $locationProvider) {
 
     $stateProvider
-      .state("initial", {
-        url: "/",
-        templateUrl: "views/initial.html"
-      })
       .state("home", {
-        url: "/home",
-        templateUrl: "views/home.html"
+        url: "/",
+        templateUrl: "/views/home.html"
       })
       .state("about", {
         url: "/about",
-        templateUrl: "views/about.html"
+        templateUrl: "/views/about.html"
       })
       .state("services", {
         url: "/services",
-        templateUrl: "views/services.html"
+        templateUrl: "/views/services.html"
       })
       .state("contact", {
         url: "/contact",
-        templateUrl: "views/contact.html"
+        templateUrl: "/views/contact.html"
       });
 
     $urlRouterProvider.otherwise("/");
-
-    $locationProvider.html5Mode(true);
   }); // end config
 })(); // end closure
