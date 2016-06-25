ebc3fa73f673a3bd75b0620048c30d063c4ea649
Daniel Mendoza <mendozad05@yahoo.com>
2016-06-24 22:46:28 -0700

    DO

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
+// app.get('/', function (req, res) {
+//   res.send('Hello World!');
+// });
+
+app.use(express.static(path.join(__dirname, '/')));
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
 
-
-app.use(express.static(path.join(__dirname, '/')));
 
 // // Listen for requests
 // var server = app.listen(app.get('port'), function() {
