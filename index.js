e4867dea44a3e2ad68840b624797199a31e5a35f
Daniel Mendoza <mendozad05@yahoo.com>
2016-06-24 22:44:23 -0700

    DO

--- index.js
+++ index.js
@@ -8,3 +8,12 @@ app.get('/', function (req, res) {
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
+
+
+app.use(express.static(path.join(__dirname, '/')));
+
+// // Listen for requests
+// var server = app.listen(app.get('port'), function() {
+//   var port = server.address().port;
+//   console.log('Magic happens on port ' + port);
+// });
